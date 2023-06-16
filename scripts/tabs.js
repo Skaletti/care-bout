3d5b3b57b3918281b899d5e3f75afc413ba4cb5a
Skaletti <gordey.korolev.2016@mail.ru>
2023-06-16 12:44:09 +0700

    bug fix tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,12 +1,12 @@
-let openCity = (evt, cityName) => {
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("requirements-tab__content");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+let openTab = (evt, cityName) => {
+    let i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("requirements-tab__content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("requirements-tab__link");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tabLinks = document.getElementsByClassName("requirements-tab__link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
     }
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";

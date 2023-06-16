9f78ea63df18fd6485f5081424445643a4ccb70f
Skaletti <gordey.korolev.2016@mail.ru>
2023-06-16 09:53:10 +0700

    styles update

---
+++ scripts/tabs.js
@@ -0,0 +1,13 @@
+let openCity = (evt, cityName) => {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("requirements-tab__content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("requirements-tab__link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
